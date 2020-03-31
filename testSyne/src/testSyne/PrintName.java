bd5922297ce14176c3ab16714b0bdf667241bbfa
vinay <vinay@DESKTOP-6HN77SJ>
2020-03-31 20:03:22 +0530

    second line of code

---
+++ testSyne/src/testSyne/PrintName.java
@@ -0,0 +1,14 @@
+package testSyne;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		
+		System.out.println("vikash kumar");
+		
+		System.out.println("second line of code");
+		
+		
+	}
+
+}
